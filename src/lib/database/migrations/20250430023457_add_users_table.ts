e7859efd7dadd9147bae91cd3e87d4e7707b95dd
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-29 23:46:12 -0300

    feat: add migration for users table with up and down methods

---
+++ src/lib/database/migrations/20250430023457_add_users_table.ts
@@ -0,0 +1,20 @@
+import { type Kysely, sql } from "kysely";
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema
+		.createTable("users")
+		.ifNotExists()
+		.addColumn("id", "serial", (col) => col.primaryKey())
+		.addColumn("name", "varchar")
+		.addColumn("email", "varchar")
+		.addColumn("password", "varchar")
+		.addColumn("created_at", "timestamp", (col) => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
+		.addColumn("updated_at", "timestamp", (col) => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
+		.execute();
+}
+
+// `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.dropTable("users").ifExists().execute();
+}
