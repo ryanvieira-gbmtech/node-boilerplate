f4d9a5ed3fe5b3866857ad86dc444b0dc3083803
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 17:29:39 -0300

    feat: add routes for sign-in with request and response validation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
+import { routes } from "./http/controllers/routes";
 import { registerAllPlugins } from "./plugins";
 
 export const app = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
 registerAllPlugins(app);
+
+app.register(routes, { prefix: "/api" });
---
+++ src/http/controllers/routes.ts
@@ -0,0 +1,19 @@
+import type { FastifyInstance } from "fastify";
+import { SignInRequest } from "./dto/request";
+import { SignInResponse } from "./dto/response";
+import { signIn } from "./sign-in";
+
+export async function routes(app: FastifyInstance) {
+	app.post(
+		"/sign-in",
+		{
+			schema: {
+				body: SignInRequest,
+				response: {
+					201: SignInResponse,
+				},
+			},
+		},
+		signIn,
+	);
+}
