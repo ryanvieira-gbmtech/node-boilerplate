e23c227b2b45b51feceb9ee8e39c421f2b5d0701
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-29 23:21:42 -0300

    chore: add environment variable schema validation using @sinclair/typebox

---
+++ src/env/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+import { Type } from "@sinclair/typebox";
+import { Value } from "@sinclair/typebox/value";
+
+const EnvSchema = Type.Object({
+	NODE_ENV: Type.Union([Type.Literal("development"), Type.Literal("production")], {
+		default: "development",
+	}),
+	DATABASE_URL: Type.String({ pattern: "^postgres://.*" }),
+	TZ: Type.String({ default: "UTC" }),
+	PORT: Type.Number({ default: 3000 }),
+});
+
+export const env = Value.Parse(EnvSchema, process.env);
