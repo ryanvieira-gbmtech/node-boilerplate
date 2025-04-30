6f775efe22b0e89d9ca4d104f1ce918015ae9d49
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 00:07:15 -0300

    feat: add @fastify/cookie dependency and register it in the app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
 import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
 
+import fastifyCookie from "@fastify/cookie";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
+
 export const app = fastify().withTypeProvider<TypeBoxTypeProvider>();
+
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: "refreshToken",
+		signed: false,
+	},
+	sign: {
+		expiresIn: "10m",
+	},
+});
+
+app.register(fastifyCookie);
