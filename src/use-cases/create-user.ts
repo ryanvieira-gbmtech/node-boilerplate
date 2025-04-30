86da1446e1c6c317c533249605d7b95571a99dc7
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 13:32:53 -0300

    feat: implement execute method in CreateUserUseCase for user creation

--- src/use-cases/create-user.ts
+++ src/use-cases/create-user.ts
@@ -1,4 +1,5 @@
 import type { UserRepository } from "@/repositories/user.repository";
+import bcryptjs from "bcryptjs";
 
 interface CreateUserInput {
 	name: string;
@@ -9,5 +10,20 @@ interface CreateUserInput {
 export class CreateUserUseCase {
 	constructor(private userRepository: UserRepository) {}
 
-	async execute(_filter: CreateUserInput) {}
+	async execute(filter: CreateUserInput) {
+		const { name, email, password } = filter;
+
+		const salt = await bcryptjs.genSalt(10);
+		const newPassword = await bcryptjs.hash(password, salt);
+
+		const newUser = await this.userRepository.create({
+			name,
+			email,
+			password: newPassword,
+		});
+
+		return {
+			user: newUser,
+		};
+	}
 }
