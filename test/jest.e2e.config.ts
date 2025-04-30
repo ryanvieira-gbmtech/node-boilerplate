52857cf04716e8c4c5f5c9ecf760c26652df7e03
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 17:53:03 -0300

    feat: add end-to-end testing configuration with Jest and include supertest dependency

---
+++ test/jest.e2e.config.ts
@@ -0,0 +1,18 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+	preset: "ts-jest",
+	rootDir: "..",
+	coverageProvider: "v8",
+	roots: ["<rootDir>/src"],
+	verbose: true,
+	passWithNoTests: true,
+	collectCoverage: false,
+	testEnvironment: "node",
+	testMatch: ["<rootDir>/src/**/test/e2e/*.test.ts"],
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/src/$1",
+	},
+};
+
+export default config;
