a7ebeabd6d09128b50106369db4a85cf76ec55b0
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 13:25:45 -0300

    feat: implement create method in UserRepository for user creation

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,13 @@
 import { db } from "@/lib/database/kysely";
+import type { NewUsers } from "@/lib/database/schema/public/Users";
 
 export class UserRepository {
+	async create(user: NewUsers) {
+		const newUser = await db.insertInto("users").values(user).returningAll().executeTakeFirst();
+
+		return newUser;
+	}
+
 	async findByEmail(email: string) {
 		const user = await db
 			.selectFrom("users")
