f7b4d95509119f18009f1886354c211aa7ad8676
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 18:05:16 -0300

    feat: implement migrate function for database schema migrations

--- test/database/migrator.ts
+++ test/database/migrator.ts
@@ -1,4 +1,8 @@
 import "dotenv/config";
+import { promises as fs } from "node:fs";
+import path from "node:path";
+import { FileMigrationProvider, Kysely, Migrator, PostgresDialect } from "kysely";
+import { Pool } from "pg";
 
 export async function changeDatabaseURL(schema: string) {
 	if (!process.env.DATABASE_URL) {
@@ -8,4 +12,36 @@ export async function changeDatabaseURL(schema: string) {
 	const url = new URL(process.env.DATABASE_URL);
 
 	url.searchParams.set("schema", schema);
+
+	await migrate(url, schema);
+}
+
+async function migrate(url: URL, schema: string) {
+	const dialect = new PostgresDialect({
+		pool: new Pool({
+			connectionString: url.toString(),
+			options: `-c search_path=${schema}`,
+		}),
+	});
+
+	const db = new Kysely({
+		dialect,
+	});
+
+	const migrator = new Migrator({
+		db,
+		migrationTableSchema: schema,
+		provider: new FileMigrationProvider({
+			fs,
+			path,
+			migrationFolder: path.join(__dirname, "../../src/lib/database/migrations"),
+		}),
+	});
+
+	const { error } = await migrator.migrateToLatest();
+
+	if (error) {
+		console.error("Migration error:", error);
+		process.exit(1);
+	}
 }
