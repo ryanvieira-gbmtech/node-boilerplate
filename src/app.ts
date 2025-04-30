09b73eee950587e7f1e47cce4b21c4c4d9c208d2
Ryan Vieira <ryanvsouza.dev@gmail.com>
2025-04-30 12:05:52 -0300

    refactor: consolidate plugin registrations into registerAllPlugins function

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,7 @@
 import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import fastify from "fastify";
-
-import fastifyCookie from "@fastify/cookie";
-import fastifyJwt from "@fastify/jwt";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import { env } from "./env";
+import { registerAllPlugins } from "./plugins";
 
 export const app = fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-app.register(fastifyJwt, {
-	secret: env.JWT_SECRET,
-	cookie: {
-		cookieName: "refreshToken",
-		signed: false,
-	},
-	sign: {
-		expiresIn: "10m",
-	},
-});
-
-app.register(fastifyCookie);
-
-app.register(fastifySwagger, {
-	openapi: {
-		info: {
-			title: "Node Boilerplate API",
-			description: "A boilerplate for building APIs with Node.js",
-			version: "1.0.0",
-		},
-	},
-});
-
-app.register(fastifySwaggerUi, {
-	routePrefix: "/docs",
-});
+registerAllPlugins(app);
